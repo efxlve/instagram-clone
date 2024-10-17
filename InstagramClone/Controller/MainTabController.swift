86818bcbc4faf50e5a27c4f4faad4f7aa70432f4
Muharrem Efe Çayırbahçe <hi@efxlve.com>
2024-10-17 15:12:32 +0300

    Configure Project

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Muharrem Efe Çayırbahçe on 17.10.2024.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
