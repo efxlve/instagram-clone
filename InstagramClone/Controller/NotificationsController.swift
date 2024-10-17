bd96d5d3d0bd0964bf353e4cb41a853dc3bd0732
Muharrem Efe Çayırbahçe <hi@efxlve.com>
2024-10-17 17:16:30 +0300

    Setup Tabbed User Interface

---
+++ InstagramClone/Controller/NotificationsController.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationController.swift
+//  InstagramClone
+//
+//  Created by Muharrem Efe Çayırbahçe on 17.10.2024.
+//
+
+import UIKit
+
+class NotificationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
+    }
+}
